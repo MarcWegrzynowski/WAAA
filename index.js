1661c908eba43e6318ae5bd5e926e550c0a9d31f
CentenoA <98501740+CentenoA@users.noreply.github.com>
2022-04-27 20:04:48 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const DiscordJS = { Client, Collection, Intents} = require('discord.js')
 const { token } = require('./config.json')
 const Discord = require("discord.js")
 const character = require ('./classes/character.js')
+const { start } = require('repl')
 
 
 //=================================================================================
@@ -41,7 +42,30 @@ for (file of commandFiles) {
 // Angels Variables
 var player = new character.character(200, 10, 'player');
 var returnObject = {returnValue: 'string'}
+var textArray = ['First Text, Press continue to see second text', 'Second text, Press continue to see third text','Third text, no button after this']
 
+var actOneArray = [
+    "You wakeup in a wagon.\nAs you start to come to you notice the wagon is smack dab in the middle of a forest and you're laying in the back full of hay.\nYou're confused, dehydrated and seem to have an aching pain on the left side of your jaw.",
+    "The Wagon Driver notices the noise you're making behind him. He doesn't seem to appreciate that you're in his wagon for free. Halfway through the trip he kicks you off the wagon to fend for yourself. You give him some rude gesture which he returns in kind, only afterwards do you think it was a bad idea realizing your now stuck in the middle of some forest.",
+    "Seeing as you have nothing better to do you start walking around the forest. You take a stop by a river to get some water and you decide to pee by nearby greenery.",
+    "As you relieve yourself you notice the greenery is making some strange noises. Suddenly you see the greenery stand up. At this point you notice it wasn't greenery... It was a goblin, and you just pissed on him. He clearly doesn't seem very happy.",
+    "The goblin is making a bunch of noises and while you can't understand him, you figured he's not saying very nice things about you. After the goblin has had his fill at yelling at you he takes out his club and decides that words are not enough and violence was the answer. You also prepare yourself to fight, grabbing a nearby pointy stick.",
+    "After dealing with the goblin and having finished your not so peaceful trip to empty the waters, you decide it's probably time to make a break out of the forest to so you can find a way back to town.",
+    "Sometime later...\n\nAfter a few hours of walking and almost losing your life after snacking on a few colorful mushrooms you found along the way, you're fairly certain that you have no idea how to get out of the forest.",
+    "Using all of your grand brilliant mind you pick up a nearby stick *that isn't your trusty weapon of choice (sharp stick)*, and you throw it to the ground,",
+    "letting it land, it points to a direction, you're not too sure if it's North or South but you decide that's the way you're gonna go until you get out of the forest. Tired and hungry, you march onward.",
+    "A few hours later...\n\nYou're still walking with no end insight of the forest and man, you are HUNGRY. Your nose twitches like an animal, swearing to you that it smells something tasty in the distance.",
+    "Losing control of your legs, you find your body limping towards that direction...",
+    "As you get closer to the smell of the food you also notice the scent and light of a campfire in the dark and next to it, a rabbit on a stick, roasting on an open fire. Practically calling your name.",
+    "You look around...\nNo one seems to be around...\nSurely one bite wouldn't hurt.\n...",
+    "In a matter of seconds the whole rabbit is gone and you're feeling great. You stand up and get ready to keep going on your way to leave the forest.",
+    "As you whistle and walk away, you bump into a pretty sturdy tree... For a tree it didn't seem so bad bumping into it.",
+    "You look up at the tree that you bumped into only to find out it wasn't a tree but a fairly upset looking hobgoblin. Needless to say you're pretty sure why he's upset. But before you can think of anything else. He's already trying murder you for eating his dinner.",
+    "You somehow manage to survive an encounter with the hobgoblin. You felt a bit bad for stealing his dinner but the empathy wore off pretty quickly after you remember him trying to murder you. Looking back you sure are glad you had that rabbit before fighting him.",
+    "Thinking about your next course of action you look up the sky taking a look at the stars. Its gotten pretty late. You decide to camp in what used to be the hobgoblin's den.",
+    `As you lay in this surprisingly comfortable pile of leaves and grasses that was a "bed" you try and get some sleep. You count some rabbits to fall asleep, getting a bit hungry again thanks to the brawl with that hobgoblin. Eventually you doze off and begin falling asleep... As you sleep, you dream that you get out of the forest and making it back to town. After that the dream gets a bit more crazy. But that's a story for another time...`,
+    "THE END",
+]
 // Marc's Variables
 let menuFlag = false;
 let townFlag = false;
@@ -73,77 +97,35 @@ client.on('messageCreate', async message => {
         menuFlag = true
         message.channel.send('Starting the game...').then(async(msg) => msg.edit('Started!'))
     }
+    /* //Functions for Testing Functionality, comment out to improve bot performance, uncomment out for testing purposes DO NOT DELETE
     if (message.content === '!storyLoop') {
         await storyLoop(message, textArray);
         await message.channel.send("Exited Continue Loop");
     }
     if (message.content === '!storyForLoop') {
+        message.channel.send("From First Index to Second Index 0-1");
         await storyForLoop(message, textArray, 2);
         await message.channel.send("Exited Continue Loop");
+        await message.channel.send("From second index to third 1-2");
+        await storyForLoop(message, textArray, 2, 1);
     }
     if (message.content === 'combatLoop') {
         let enemy = new character.character(200, 10, 'goblin')
         combatLoop(message, player, enemy)
     }
+    */
     if (message.content === '!demo') {
         let goblin = new character.character(100, 10, 'goblin');
         let hobGob = new character.character(200, 14, 'Hobgoblin');
 
-        await message.channel.send("You wakeup in a wagon.\nAs you start to come to you notice the wagon is smack dab in the middle of a forest and you're laying in the back full of hay.\nYou're confused, dehydrated and seem to have an aching pain on the left side of your jaw.\n\n");
-        await delay(1000*5) //5 seconds
-        await message.channel.send("The Wagon Driver notices the noise you're making behind him. He doesn't seem to appreciate that you're in his wagon for free. Halfway through the trip he kicks you off the wagon to fend for yourself. You give him some rude gesture which he returns in kind, only afterwards do you think it was a bad idea realizing your now stuck in the middle of some forest.\n\n")
-        await delay(1000*5) //5 seconds
-        await message.channel.send("Seeing as you have nothing better to do you start walking around the forest. You take a stop by a river to get some water and you decide to pee by nearby greenery.");
-        await delay(1000*5) //5 seconds
-        await message.channel.send("As you relieve yourself you notice the greenery is making some strange noises. Suddenly you see the greenery stand up. At this point you notice it wasn't greenery... It was a goblin, and you just pissed on him. He clearly doesn't seem very happy.\n\n");
-        await delay(1000*10) //5 seconds
-        await message.channel.send("The goblin is making a bunch of noises and while you can't understand him, you figured he's not saying very nice things about you. After the goblin has had his fill at yelling at you he takes out his club and decides that words are not enough and violence was the answer. You also prepare yourself to fight, grabbing a nearby pointy stick.\n\n");
-        await delay(1000*10) //3 seconds
+        await storyForLoop(message, actOneArray, 4) // 4 iterations
         await combatLoop(message, player, goblin);
-        if (player.HP > 0) {
-            await delay(1000*10) //3 seconds
-            await message.channel.send("After dealing with the goblin and having finished your not so peaceful trip to empty the waters, you decide it's probably time to make a break out of the forest to so you can find a way back to town.\n\n");
-            await delay(1000*10) //2 seconds
-            await message.channel.send("Sometime later...\n\n")
-            await delay(1000*5) //5 seconds
-            await message.channel.send("After a few hours of walking and almost losing your life after snacking on a few colorful mushrooms you found along the way, you're fairly certain that you have no idea how to get out of the forest.\n\n");
-            await delay(1000*10) //5 seconds
-            await message.channel.send("Using all of your grand brilliant mind you pick up a nearby stick *that isn't your trusty weapon of choice (sharp stick)*, and you throw it to the ground,"); 
-            await delay(1000*10) //5 seconds
-            await message.channel.send("letting it land, it points to a direction, you're not too sure if it's North or South but you decide that's the way you're gonna go until you get out of the forest. Tired and hungry, you march onward.\n\n");
-            await delay(1000*10) //5 seconds
-            await message.channel.send("A few hours later...\n\n");
-            await delay(1000*10) //5 seconds
-            await message.channel.send("You're still walking with no end insight of the forest and man, you are HUNGRY. Your nose twitches like an animal, swearing to you that it smells something tasty in the distance.");
-            await delay(1000*10) //4 seconds
-            await message.channel.send("Losing control of your legs, you find your body limping towards that direction...\n\n");
-            await delay(1000*10) //3 seconds
-            await message.channel.send("As you get closer to the smell of the food you also notice the scent and light of a campfire in the dark and next to it, a rabbit on a stick, roasting on an open fire. Practically calling your name.\n\n");
-            await delay(1000*10) //5 seconds
-            await message.channel.send("You look around... ");
-            await delay(1000*5) //2 seconds
-            await message.channel.send("No one seems to be around...");
-            await delay(1000*5) //2 seconds
-            await message.channel.send("Surely one bite wouldn't hurt.");
-            await delay(1000*5) //2 seconds
-            await message.channel.send("...\n\n");
-            await delay(1000*2) //2 seconds
-            await message.channel.send("In a matter of seconds the whole rabbit is gone and you're feeling great. You stand up and get ready to keep going on your way to leave the forest.");
-            await delay(1000*10) //3 seconds
-            await message.channel.send("As you whistle and walk away, you bump into a pretty sturdy tree... For a tree it didn't seem so bad bumping into it.\n\n");
-            await delay(1000*10) //3 seconds
-            await message.channel.send("You look up at the tree that you bumped into only to find out it wasn't a tree but a fairly upset looking hobgoblin. Needless to say you're pretty sure why he's upset. But before you can think of anything else. He's already trying murder you for eating his dinner.\n\n");
-            await delay(1000*10) //5 seconds
+        if (player.HP > 0) { // index 5 to 15, 10 iterations
+            await storyForLoop(message, actOneArray, 10, 5) //starts on index 5, 10 iterations
             await combatLoop(message, player, hobGob);
         }
-        if (player.HP > 0) {
-            await message.channel.send("You somehow manage to survive an encounter with the hobgoblin. You felt a bit bad for stealing his dinner but the empathy wore off pretty quickly after you remember him trying to murder you. Looking back you sure are glad you had that rabbit before fighting him.\n\n");
-            await delay(1000*10) //5 seconds
-            await message.channel.send("Thinking about your next course of action you look up the sky taking a look at the stars. Its gotten pretty late. You decide to camp in what used to be the hobgoblin's den.\n\n");
-            await delay(1000*10) //5 seconds
-            await message.channel.send(`As you lay in this surprisingly comfortable pile of leaves and grasses that was a "bed" you try and get some sleep. You count some rabbits to fall asleep, getting a bit hungry again thanks to the brawl with that hobgoblin. Eventually you doze off and begin falling asleep... As you sleep, you dream that you get out of the forest and making it back to town. After that the dream gets a bit more crazy. But that's a story for another time...\n\n`)
-            await delay(1000*10) //5 seconds
-            await message.channel.send("THE END")
+        if (player.HP > 0) { //index 16- , 4 iterations
+            await storyForLoop(message, actOneArray, 4, 16) //starts on index 16, 4  iterations
         }
     }
     else if (message.content === '!status') {
@@ -187,8 +169,6 @@ client.on('messageUpdate', message => {
 //=================================================================================
 
 
-var textArray = ['First Text, Press continue to see second text', 'Second text, Press continue to see third text','Third text, no button after this']
-
 async function storyLoop(message, array) {
     let story = await message.channel.send("``` ```");
     let allowButtonsFlag = true;
@@ -212,11 +192,11 @@ async function storyLoop(message, array) {
     returnObject.returnValue = 'string';
 }
 
-async function storyForLoop(message, array, iterations) {
+async function storyForLoop(message, array, iterations, startIndex = 0) {
     let story = await message.channel.send("``` ```");
     let allowButtonsFlag = true;
     const continueB = client.commands.get("continueButton");
-    for (let i=0; i < iterations; i++) {
+    for (let i = startIndex; i < (iterations + startIndex + 1); i++) {
         await continueB.run(client, message, returnObject, allowButtonsFlag);
         await delay(1000*10) //10 seconds to click
         console.log(returnObject.returnValue);
